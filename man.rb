3e118c8791a7446bf72ef6f6eb4f70e1ba326f4b
Jacob Coble <jacob.coble@openmailbox.org>
2014-05-14 20:08:30 -0400

    Add jumping to Man

--- man.rb
+++ man.rb
@@ -25,7 +25,29 @@ class Man
   def slide(pos)
     self.valid_moves.include?(pos) && self.position = pos
   end
-  
+
+  def jump(pos)
+    row_diff = pos.first - self.position.first
+    col_diff = pos.last  - self.position.last
+    delta    = [row_diff, col_diff]
+
+    if row_diff == -2 && col_diff.abs == 2 && self.board[pos].nil? && self.board[pos].color != self.color
+      target_row = self.position.first 
+      target_col = self.position.last
+
+      #FIXME: Move target removal to board function?
+      target = self.board[target_row][target_col]
+      self.board.men[target.color].delete(target)
+      target.position = nil
+
+      self.position = pos
+      true
+    else
+      false
+    end
+
+  end
+
   def valid_moves
     deltas = DELTAS
 
@@ -35,6 +57,8 @@ class Man
     end
 
     positions.select { |position| self.board[position].nil? }
+  end
+
   end
   
   def to_s
