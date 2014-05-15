5d4fac981e74abc80e9dbd6a3206de9420c0f7f4
Jacob Coble <jacob.coble@openmailbox.org>
2014-05-15 10:36:11 -0400

    Remove needless method

--- board.rb
+++ board.rb
@@ -16,10 +16,6 @@ class Board
     puts self
   end
   
-  def move(start, target)
-    
-  end
-  
   def to_s
     string = "   0  1  2  3  4  5  6  7  8  9\n"
     background_color = self.starting_color
